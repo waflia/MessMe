312547908abc005c257ba05b24b056e6c24c8648
Waflia <waflias@yandex.ru>
2020-10-17 14:59:07 +0300

    Created FAB click handler

--- app/src/main/java/com/waflia/messme/MainActivity.java
+++ app/src/main/java/com/waflia/messme/MainActivity.java
@@ -1,8 +1,7 @@
 package com.waflia.messme;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -10,31 +9,23 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
-import com.waflia.messme.RandomUserAPI.Model.RandomUserResponse;
 import com.waflia.messme.RandomUserAPI.Model.Result;
-import com.waflia.messme.RandomUserAPI.RandomAPIService;
-import com.waflia.messme.chat.ChatFragment;
-import com.waflia.messme.dialogs.DialogFragment;
 import com.waflia.messme.dialogs.DialogRecyclerViewAdapter;
 import com.waflia.messme.dialogs.DialogViewModel;
-import com.waflia.messme.dialogs.UserDialog;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.moshi.MoshiConverterFactory;
 
 import static com.waflia.messme.chat.ChatFragment.SIGN_IN_CODE;
 
@@ -53,6 +44,17 @@ public class MainActivity extends AppCompatActivity {
 
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
         checkFirebaseAuth();
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(findViewById(R.id.main_fragment), "Fab clicked", Snackbar.LENGTH_SHORT)
+                        .setBackgroundTint(getColor(R.color.colorMessageView))
+                        .setTextColor(Color.BLACK)
+                        .show();
+            }
+        });
 
         RecyclerView recyclerView = findViewById(R.id.dialog_list);
         RecyclerView.LayoutManager manager = new LinearLayoutManager(this);
